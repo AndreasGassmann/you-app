944c3618e72e2c888cb54cbce8d1c817862d8410
Pascal Brun <brunpasc@gmail.com>
2019-08-24 17:36:09 +0200

    feat(clients): clients page styling

--- src/app/clients/clients.module.ts
+++ src/app/clients/clients.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { clientsPage } from "./clients.page";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    FontAwesomeModule,
     RouterModule.forChild([{ path: "", component: clientsPage }])
   ],
   declarations: [clientsPage]
